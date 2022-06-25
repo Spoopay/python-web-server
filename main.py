bc22c8433a2f9fa12cc2247f72cb7f234b250d4c
HexNation <idc@idc.com>
2022-06-25 00:43:29 -0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import socket
+import threading
 import json
+from wsgiref.simple_server import server_version
 
 PORT = 25568
 FORMAT = 'utf-8'
@@ -9,18 +11,33 @@ def send(msg):
     message = msg.encode(FORMAT)
     client.send(message)
 
+def getUserMessage():
+    connected = True
+    while connected:
+        user_message = encodeToJson(input(), username)
+        send(user_message)
+
 def encodeToJson(message, username):
     json_data = {}
     json_data["username"] = username
     json_data["message"] = message
     return json.dumps(json_data)
 
+def decodeFromJson(data):
+    if (data == "DISCONNECTED"):
+        return False
+    return json.loads(data)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
     username = input("Input an username: ")
     client.connect((SERVER, PORT))
     print(f"Welcome to the server {username}!")
     connected = True
+    thread = threading.Thread(target=getUserMessage, args=())
+    thread.start()
     while connected:
-        user_message = encodeToJson(input(), username)
-        send(user_message)
+        server_msg = decodeFromJson(client.recv(1024).decode())
+        if (server_msg == False):
+            break
+        if (server_msg['username'] != username):
+            print(f"[{server_msg['username']}] {server_msg['message']}")
